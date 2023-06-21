4d7c7e7ce23917da0b1c2516bfdcd838b6b5ec14
Elkana Lang'at <elkanahlangatt@gmail.com>
2023-06-21 17:27:07 +0300

    first commit

---
+++ src/test/java/com/langat/h2demo/H2DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.langat.h2demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class H2DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
